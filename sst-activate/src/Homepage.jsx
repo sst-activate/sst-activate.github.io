9fef32dbea47da2ccd6d8ff098f787cbf2b49cb0
YeohTH <soundwavedecepticonleader@gmail.com>
2024-02-02 22:23:12 +0800

    Checks

--- sst-activate/src/Homepage.jsx
+++ sst-activate/src/Homepage.jsx
@@ -10,6 +10,13 @@ function Homepage(){
                 <div className='topNavButtons'>Organisations</div>
                 <div className='topNavButtons'>Past Projects</div>
             </div>
+            <div id='introPara'>
+            <h1>Hello SST!</h1>
+            In this website you will find a collection of articles, list of voluntary welfare organisations and initiatives, 
+            service learning projects that students embarked on in the past, as well as guides on how you may do the same.
+            May these widen your horizons on what is happening in our community and world, inspire you to take action, 
+            and in turn help make this place we are living in a better one (:
+            </div>
         </>
     )
 }
