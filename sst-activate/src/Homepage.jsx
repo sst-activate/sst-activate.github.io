da908b28fd2c0d0a4d066cc71047cb2abbbdac3c
YeohTH <soundwavedecepticonleader@gmail.com>
2024-02-04 13:08:12 +0800

    hover effect

--- sst-activate/src/Homepage.jsx
+++ sst-activate/src/Homepage.jsx
@@ -6,16 +6,28 @@ function Homepage(){
         <>
             <div className="topNav">
                 <img src={ACELOGO} id="aceLogo"></img>
-                <div className='topNavButtons'>Home</div>
+                <div className='topNavButtons-active'>Home</div>
                 <div className='topNavButtons'>Organisations</div>
                 <div className='topNavButtons'>Past Projects</div>
             </div>
             <div id='introPara'>
-            <h1>Hello SST!</h1>
+            <h1><span className='wordHovering'>H</span>
+            <span className='wordHovering'>e</span>
+            <span className='wordHovering'>l</span>
+            <span className='wordHovering'>l</span>
+            <span className='wordHovering'>o</span>
+            <span className='wordHovering'> </span>
+            <span className='wordHovering'>S</span>
+            <span className='wordHovering'>S</span>
+            <span className='wordHovering'>T</span>
+            <span className='wordHovering'>!</span>
+            </h1>
+            <p style={{margin:"5vw"}}>
             In this website you will find a collection of articles, list of voluntary welfare organisations and initiatives, 
             service learning projects that students embarked on in the past, as well as guides on how you may do the same.
             May these widen your horizons on what is happening in our community and world, inspire you to take action, 
             and in turn help make this place we are living in a better one (:
+            </p>
             </div>
         </>
     )
